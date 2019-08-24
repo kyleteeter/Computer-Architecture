2bf2348451722e8e0a17ee27c195c196d41ff8d7
unknown <kyleteeter12@gmail.com>
2019-08-24 12:32:02 -0500

    Defined read and write

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -12,10 +12,10 @@ class CPU:
         self.pc = pc
 
     def ram_read(self, pc_address):
-        pass
+        return self.ram[pc_address]
 
     def ram_write(self, value, pc_address):
-        pass
+        self.ram[pc_address] = value
 
     def load(self):
         """Load a program into memory."""
@@ -70,4 +70,4 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        
